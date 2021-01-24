a1363d600df718aa4b6e5a5c4b0cb6075358899a
Borislav Grigorov <bobi@noblehire.io>
2021-01-24 19:00:23 +0200

    scaffold Header component + .prettierrc file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+  Redirect,
+} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/dashboard">
+          <DashboardPage />
+        </Route>
         <Route path="/login">
           <LoginPage />
         </Route>
         <Route path="/register">
           <RegisterPage />
         </Route>
+        <Redirect to="/dashboard" />
       </Switch>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
