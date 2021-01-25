34d132ec9cd4fa8d619dfa5d881462ef4469f125
Borislav Grigorov <bobi@noblehire.io>
2021-01-25 12:23:00 +0200

    remove unused classNames

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -11,8 +11,8 @@ export default function MeetingCard({ data }: { data: Meeting }) {
   return (
     <div className="border border-gray-300 rounded bg-white p-4 hover:shadow-md hover:border-indigo-500">
       <h1 className="font-bold text-xl">{data.title}</h1>
-      <div className="">25 Jan 21, Monday</div>
-      <blockquote className="text-gray-800">{data.excerpt}</blockquote>
+      <div>25 Jan 21, Monday</div>
+      <blockquote>{data.excerpt}</blockquote>
 
       <div className="flex justify-between mt-4">
         <div />
