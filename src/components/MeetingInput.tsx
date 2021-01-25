23c85aa966289be47342a332f2bcf02e504178f8
Borislav Grigorov <bobi@noblehire.io>
2021-01-25 19:52:53 +0200

    handle dark mode

--- src/components/MeetingInput.tsx
+++ src/components/MeetingInput.tsx
@@ -4,7 +4,7 @@ export default function MeetingInput() {
   return (
     <div className="relative w-full">
       <div className="w-full">
-        <div className="border-t border-gray-300 bg-white p-4">
+        <div className="border-t border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 p-4">
           <Input type="text" />
         </div>
       </div>
