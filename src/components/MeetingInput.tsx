96eb65da35ee36c12ad69b8c98a0c021f7432e24
Borislav Grigorov <bobi@noblehire.io>
2021-01-26 19:49:58 +0200

    fix scrolling the meeting input container

--- src/components/MeetingInput.tsx
+++ src/components/MeetingInput.tsx
@@ -4,17 +4,27 @@ import MeetingInputField from './MeetingInputField'
 import React, { useState } from 'react'
 import { Container } from './core/Container'
 import Title from './core/Title'
+import {
+  MEETING_INPUT_FIELD_SECTION_HEIGHT,
+  MEETING_INPUT_HEADING_SECTION_HEIGHT,
+} from '../constants'
 
 export default function MeetingInput() {
   const [inputs] = useState([
     { id: 1, text: 'Lorem ipsum dolor sit amet' },
     { id: 2, text: 'Lorem ipsum dolor sit amet' },
     { id: 3, text: 'Lorem ipsum dolor sit amet' },
+    { id: 4, text: 'Lorem ipsum dolor sit amet' },
+    { id: 5, text: 'Lorem ipsum dolor sit amet' },
+    { id: 6, text: 'Lorem ipsum dolor sit amet' },
   ])
 
   return (
     <div className="flex flex-col relative border-r border-gray-300 dark:border-gray-700">
-      <div className="bg-white dark:bg-gray-800 border-b border-gray-300 dark:border-gray-700">
+      <div
+        style={{ height: MEETING_INPUT_HEADING_SECTION_HEIGHT }}
+        className="bg-white dark:bg-gray-800 border-b border-gray-300 dark:border-gray-700"
+      >
         <Container>
           <div className="flex justify-between items-center">
             <Title bg pushBottom={false}>
@@ -27,7 +37,10 @@ export default function MeetingInput() {
       <div className="relative flex-1 flex justify-end flex-col">
         <MeetingInputList inputs={inputs} />
       </div>
-      <div>
+      <div
+        className="overflow-hidden"
+        style={{ height: MEETING_INPUT_FIELD_SECTION_HEIGHT }}
+      >
         <MeetingInputField />
       </div>
     </div>
--- src/components/MeetingInputList.tsx
+++ src/components/MeetingInputList.tsx
@@ -1,5 +1,9 @@
 import { Container } from './core/Container'
 import Button, { ButtonSize } from './core/Button'
+import {
+  MEETING_INPUT_FIELD_SECTION_HEIGHT,
+  MEETING_INPUT_HEADING_SECTION_HEIGHT,
+} from '../constants'
 
 interface MeetingInput {
   id: number
@@ -12,25 +16,34 @@ export default function MeetingInputList({
   inputs: MeetingInput[]
 }) {
   return (
-    <Container>
-      <div className="flex flex-col justify-end">
-        {inputs.map((input) => {
-          return (
-            <div
-              key={input.id}
-              className="border border-gray-300 dark:border-gray-700 mt-4 rounded overflow-hidden"
-            >
-              <div className="p-4 bg-white dark:bg-gray-800">{input.text}</div>
-              <div className="flex items-center px-4 p-2.5 bg-white dark:bg-gray-800 border-t border:gray-100 dark:border-gray-700">
-                <Button size={ButtonSize.sm} className="mr-2">
-                  To note
-                </Button>
-                <Button size={ButtonSize.sm}>To next step</Button>
+    <div
+      className="overflow-auto"
+      style={{
+        height: `calc(100vh - ${MEETING_INPUT_HEADING_SECTION_HEIGHT}px - ${MEETING_INPUT_FIELD_SECTION_HEIGHT}px`,
+      }}
+    >
+      <Container>
+        <div className="flex flex-col justify-end">
+          {inputs.map((input) => {
+            return (
+              <div
+                key={input.id}
+                className="border border-gray-300 dark:border-gray-700 mt-4 rounded overflow-hidden"
+              >
+                <div className="p-4 bg-white dark:bg-gray-800">
+                  {input.text}
+                </div>
+                <div className="flex items-center px-4 p-2.5 bg-white dark:bg-gray-800 border-t border:gray-100 dark:border-gray-700">
+                  <Button size={ButtonSize.sm} className="mr-2">
+                    To note
+                  </Button>
+                  <Button size={ButtonSize.sm}>To next step</Button>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>
-    </Container>
+            )
+          })}
+        </div>
+      </Container>
+    </div>
   )
 }
