b921e6a065bc6fe5c72c2a589169f8ab2df87463
Borislav Grigorov <bobi@noblehire.io>
2021-01-26 17:21:57 +0200

    fix MeetingCard UI defect with the open button

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -9,14 +9,14 @@ interface Meeting {
 
 export default function MeetingCard({ data }: { data: Meeting }) {
   return (
-    <div className="border border-gray-300 dark:border-gray-700 rounded bg-white dark:bg-gray-800 p-4 hover:shadow-md hover:border-indigo-500 dark:hover:border-indigo-300">
-      <h1 className="font-bold text-xl">{data.title}</h1>
-      <div>25 Jan 21, Monday</div>
-      <blockquote>{data.excerpt}</blockquote>
-
+    <div className="flex flex-col justify-end border border-gray-300 dark:border-gray-700 rounded bg-white dark:bg-gray-800 p-4 hover:shadow-md hover:border-indigo-500 dark:hover:border-indigo-300">
+      <div className="flex-1">
+        <h1 className="font-bold text-xl">{data.title}</h1>
+        <div>25 Jan 21, Monday</div>
+        <blockquote>{data.excerpt}</blockquote>
+      </div>
       <div className="flex justify-between mt-4">
         <div />
-
         <LinkButton to="/m/1" type="button">
           Open
         </LinkButton>
