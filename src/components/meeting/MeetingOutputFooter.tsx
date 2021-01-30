1ef9b6d0b798d313910e2af334d6533cf5001a23
Borislav Grigorov <bobi@noblehire.io>
2021-01-30 18:32:15 +0200

    changing output sections from the output footer

--- src/components/meeting/MeetingOutputFooter.tsx
+++ src/components/meeting/MeetingOutputFooter.tsx
@@ -1,25 +1,62 @@
 import React from 'react'
 import { SECTION_FOOTER_MIN_HEIGHT } from '../../constants'
+import cn from '../../utils/cn'
 
-function FooterButton({ children }: { children: React.ReactNode }) {
+export enum MeetingOutputFooterSections {
+  Notes = 'Notes',
+  NextSteps = 'NextSteps',
+}
+
+function FooterButton({
+  isActive,
+  children,
+  onClick,
+}: {
+  isActive: boolean
+  children: React.ReactNode
+  onClick: () => void
+}) {
   return (
     <div
-      className="flex items-center justify-center cursor-pointer border-t-4 border-gray-300 dark:border-gray-700 hover:border-indigo-500 dark:hover:border-indigo-500 px-4"
-      style={{ height: '100%' }}
+      onClick={onClick}
+      className={cn({
+        'flex items-center justify-center cursor-pointer border-t-4 px-4': true,
+        'border-gray-300 dark:border-gray-700 hover:border-indigo-500 dark:hover:border-indigo-500': !isActive,
+        'border-indigo-400': isActive,
+      })}
+      style={{
+        height: '100%',
+      }}
     >
       {children}
     </div>
   )
 }
 
-export default function MeetingOutputFooter() {
+export default function MeetingOutputFooter({
+  activeSection,
+  onSectionChange,
+}: {
+  activeSection: MeetingOutputFooterSections
+  onSectionChange: (section: MeetingOutputFooterSections) => void
+}) {
   return (
     <div
       className="flex items-center relative bg-white dark:bg-gray-800 border-t border-gray-300 dark:border-gray-700"
       style={{ height: SECTION_FOOTER_MIN_HEIGHT }}
     >
-      <FooterButton>Notes</FooterButton>
-      <FooterButton>Next steps</FooterButton>
+      <FooterButton
+        isActive={activeSection === MeetingOutputFooterSections.Notes}
+        onClick={() => onSectionChange(MeetingOutputFooterSections.Notes)}
+      >
+        Notes
+      </FooterButton>
+      <FooterButton
+        isActive={activeSection === MeetingOutputFooterSections.NextSteps}
+        onClick={() => onSectionChange(MeetingOutputFooterSections.NextSteps)}
+      >
+        Next steps
+      </FooterButton>
     </div>
   )
 }
--- src/components/meeting/Output.tsx
+++ src/components/meeting/Output.tsx
@@ -1,17 +1,42 @@
 import MeetingSection from './MeetingSection'
-import MeetingOutputFooter from './MeetingOutputFooter'
+import MeetingOutputFooter, {
+  MeetingOutputFooterSections,
+} from './MeetingOutputFooter'
 import OutputEntityContextMenu from '../entities/contextMenu/OutputEntityContextMenu'
-import React from 'react'
+import React, { useState } from 'react'
 import { useEntities } from '../entities/EntitiesProvider'
+import { EntityOutputType, IEntity } from '../entities/Entity'
 
 export default function Output() {
   const { items } = useEntities()
+  const [activeSection, setActiveSection] = useState(
+    MeetingOutputFooterSections.Notes
+  )
+
+  const filter = (
+    items: IEntity[],
+    activeSection: MeetingOutputFooterSections
+  ) => {
+    const map = {
+      [EntityOutputType.Note]: MeetingOutputFooterSections.Notes,
+      [EntityOutputType.NextStep]: MeetingOutputFooterSections.NextSteps,
+    }
+    return items.filter((item) => {
+      if (!item.output) return null
+      return map[item.output.type] === activeSection
+    })
+  }
 
   return (
     <MeetingSection
       title="Output"
-      entities={items.filter((item) => item.output !== null)}
-      footer={<MeetingOutputFooter />}
+      entities={filter(items, activeSection)}
+      footer={
+        <MeetingOutputFooter
+          activeSection={activeSection}
+          onSectionChange={(section) => setActiveSection(section)}
+        />
+      }
       contextMenu={(entity) => <OutputEntityContextMenu entity={entity} />}
     />
   )
